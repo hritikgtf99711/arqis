24ea27519a873c9369fe045d608a22946fcf9902
Hritik <hritik.kumar@gtftechnologies.com>
2025-09-30 18:26:49 +0530

    changes

--- src/app/utils/gsapAnimations.js
+++ src/app/utils/gsapAnimations.js
@@ -8,23 +8,22 @@ export const initScrollSmoother = () => {
   const smoother = ScrollSmoother.create({
     wrapper: "#smooth-wrapper",
     content: "#smooth-content",
-    smooth: 1,
+    smooth: 1.5,
     effects: true,
   });
-
   const horizontalSection = document.querySelector(".horizontal-section");
   if (horizontalSection) {
     const slides = gsap.utils.toArray(".horizontal-section .item");
     const snapPoints = slides.map((slide, i) => i / (slides.length - 1));
-
-    const tl = gsap.timeline({
+    gsap.to(slides, {
+      xPercent: -100 * (slides.length - 1),
+      ease: "none",
       scrollTrigger: {
         trigger: horizontalSection,
         start: "top top",
         end: () => "+=" + (horizontalSection.scrollWidth - window.innerWidth),
         pin: true,
         scrub: 1,
-        stagger:0.1,
         snap: {
           snapTo: snapPoints,
           duration: { min: 0.2, max: 0.5 },
@@ -35,11 +34,6 @@ export const initScrollSmoother = () => {
           const currentSlide = Math.round(self.progress * (slides.length - 1));
         },
       },
-    });
-
-    tl.to(slides, {
-      ease: "none",
-      opacity:0
     });
   }
 
