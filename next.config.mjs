af7abb8cee2c1edf75a6120a87f0ebf6ee5eacba
Hritik <hritik.kumar@gtftechnologies.com>
2025-10-06 16:19:34 +0530

    chnages

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    eslint: {
-        // Ignore ESLint errors during build
-        ignoreDuringBuilds: true,
-    },
-    typescript: {
-        // Ignore TypeScript errors during build
-        ignoreBuildErrors: true,
-    },
+    
 };
 
 export default nextConfig;
