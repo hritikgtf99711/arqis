5825a6a6f321347c92ae6021e22ddce8c104a3b3
Hritik <hritik.kumar@gtftechnologies.com>
2025-10-06 16:55:00 +0530

    changes

--- src/app/HorizontalLayout.js
+++ src/app/HorizontalLayout.js
@@ -5,7 +5,7 @@ import AbsSec from "./components/common/absSec";
 import Footer from "./components/common/footer";
 import { useLayoutEffect, useState } from "react";
 import ScrollContext from "./context/ScrollContext";
-import InitScrollSmoother from "./utils/GsapAnimations";
+import InitScrollSmoother from "./utils/gsapAnimations";
 
 export default function HorizontalLayout({ children }) {
   const [gotoFn, setgoTofn] = useState(0);
