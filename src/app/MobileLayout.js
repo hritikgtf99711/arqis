9750814233e77749167d69144bc5554572b14dce
Hritik <hritik.kumar@gtftechnologies.com>
2025-10-14 14:56:40 +0530

    css changes

--- src/app/MobileLayout.js
+++ src/app/MobileLayout.js
@@ -13,43 +13,43 @@ import Pagination from "./utils/Pagination";
 export default function MobileLayout() {
   return (
     <MobHorizontalLayout>
-      <div className="flex h-[100vh] overflow-x-scroll pt-[100px] horizontal-section">
+      <div className="flex h-[100vh] overflow-hidden pt-[100px] horizontal-section">
         <div className="basis-[100%] item grow-0 shrink-0 h-full">
           <LogoSection />
         </div>
         <div
           data-scroll="vertical"
-          className="basis-[100%] overflow-y-scroll mob_scroll   item grow-0 shrink-0 h-full"
+          className="basis-[100%] overflow-y-scroll overflow-x-hidden mob_scroll   item grow-0 shrink-0 h-full"
         >
           <About  mobVia={1} />
           <Pagination/>
           <FooterContainer />
         </div>
-        <div className="basis-[100%]  item grow-0 mob_scroll  overflow-y-scroll  shrink-0 h-full">
+        <div className="basis-[100%]  item grow-0 mob_scroll  overflow-y-scroll overflow-x-hidden  shrink-0 h-full">
           <ProjectContainer />
            <Pagination/>
           <FooterContainer />
         </div>
-        {/* <div className="basis-[100%]  item grow-0  overflow-y-scroll  shrink-0 h-full  items-center">
+        {/* <div className="basis-[100%]  item grow-0  overflow-y-scroll overflow-x-hidden  shrink-0 h-full  items-center">
           <TeamContainer />
           <FooterContainer />
         </div> */}
-        <div className="basis-[100%]  overflow-y-scroll mob_scroll item grow-0 shrink-0 h-full">
+        <div className="basis-[100%]  overflow-y-scroll overflow-x-hidden mob_scroll item grow-0 shrink-0 h-full">
           <CareerContainer />
            <Pagination/>
           <FooterContainer />
         </div>
-        <div className="basis-[100%]  overflow-y-scroll mob_scroll item grow-0 shrink-0 h-full  items-center">
+        <div className="basis-[100%]  overflow-y-scroll overflow-x-hidden mob_scroll item grow-0 shrink-0 h-full  items-center">
           <MediaContainer />
            <Pagination/>
           <FooterContainer />
         </div>
-        <div className="basis-[100%] item  overflow-y-scroll mob_scroll  grow-0 shrink-0 h-full">
+        <div className="basis-[100%] item  overflow-y-scroll overflow-x-hidden mob_scroll  grow-0 shrink-0 h-full">
           <BlogContainer />
            <Pagination/>
           <FooterContainer />
         </div>
-        <div className="basis-[100%]  overflow-y-scroll  mob_scroll item grow-0 shrink-0 h-full">
+        <div className="basis-[100%]  overflow-y-scroll overflow-x-hidden  mob_scroll item grow-0 shrink-0 h-full">
           <ContactformContainer />
            <Pagination/>
           <FooterContainer />
--- src/app/page.js
+++ src/app/page.js
@@ -1,24 +1,12 @@
 "use client";
-import { useState, useEffect } from "react";
 import DesktopLayout from "./DesktopLayout";
 import MobileLayout from "./MobileLayout";
-
 export default function Home() {
-  const [isMobile, setIsMobile] = useState(null); 
-  useEffect(() => {
-    const handleResize = () => {
-      setIsMobile(window.innerWidth < 768);
-    };
-    handleResize();
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
-  if (isMobile === null) return null;
-
   return (
     <>
-      {isMobile ? <MobileLayout /> : <DesktopLayout />}
-    </>
+        {/* <DesktopLayout customClass={`lg:block hidden`}/> */}
+        <MobileLayout  customClass={`lg:hidden block`}/>
+        
+      </>
   );
 }
