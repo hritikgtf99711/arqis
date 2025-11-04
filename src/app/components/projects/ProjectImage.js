b317b77aa31a2c6bfd7eb02aa99cd4186277ce48
Hritik <hritik.kumar@gtftechnologies.com>
2025-11-04 11:15:20 +0530

    changes

--- src/app/components/projects/ProjectImage.js
+++ src/app/components/projects/ProjectImage.js
@@ -4,7 +4,7 @@ import React from 'react'
 export default function ProjectImage() {
   return (
     <div>
-      <Image className='w-full h-[320px] lg:h-full object-cover arrow' src={'/assets/projects/project_1.jpg'} width={500} height={500} alt='projectimage'/>
+      <Image className='w-full lg:h-full object-cover arrow' src={'/assets/projects/project_1.jpg'} width={500} height={500} alt='projectimage'/>
     </div>
   )
 }
