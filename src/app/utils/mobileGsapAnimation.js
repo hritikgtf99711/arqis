e6ac85dd34e4da1dfd80d254aa43b50890000a2b
Hritik <hritik.kumar@gtftechnologies.com>
2025-11-06 16:58:22 +0530

    swiper issue 2nd fixed

--- src/app/utils/mobileGsapAnimation.js
+++ src/app/utils/mobileGsapAnimation.js
@@ -35,7 +35,7 @@ export default function initScrollSmoother(router) {
   const CONFIG = {
     COOLDOWN_MS: 100,
     ANIM_DURATION: 1.1,
-    TOUCH_THRESH: 500,
+    TOUCH_THRESH: 130,
     DEBOUNCE_MS: 90,
     SCROLL_SPEED: 0.4,
   };
