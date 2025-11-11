9a500772af2f6d1510fc54c64c0a390f6c9a18d6
Hritik <hritik.kumar@gtftechnologies.com>
2025-11-11 10:51:37 +0530

    micrositeadded

---
+++ src/app/(website)/[microsite]/page.js
@@ -0,0 +1,17 @@
+import React from "react";
+import HorizontalLayout from "@/app/HorizontalLayout";
+// import
+export default function page() {
+  return (
+    <HorizontalLayout>
+      <div className="flex h-[100vh] overflow-x-scroll horizontal-section">
+        <div className="basis-[100%] flex items-center justify-center  item grow-0 shrink-0 h-full">
+          <HomeBanner />
+        </div>
+         <div className="basis-[100%] flex items-center justify-center  item grow-0 shrink-0 h-full">
+          <HomeBanner />
+        </div>
+      </div>
+    </HorizontalLayout>
+  );
+}
